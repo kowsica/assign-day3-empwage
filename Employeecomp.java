078eb5ff605d5f15d8ba23c2a94ba53ac67f4b33
kowsica <kowsica97@gmail.com>
2023-01-20 19:30:36 +0530

    UC-6 added

--- Employeecomp.java
+++ Employeecomp.java
@@ -2,33 +2,34 @@ package empwagecomp;
 
 public class Employeecomp {
 	
-	    
-	 public static final int IS_PART_TIME = 1;
-	    public static final int IS_FULL_TIME = 2;
-	    public static final int EMP_RATE_PER_HOUR = 20;
-	    public static final int NUM_OF_WORKING_DAYS = 20;
+	public static final int IS_PART_TIME = 1;
+    public static final int IS_FULL_TIME = 2;
+    public static final int EMP_RATE_PER_HOUR = 20;
+    public static final int NUM_OF_WORKING_DAYS = 20;
+    public static final int MAX_HRS_IN_MONTH = 10;
 
-	    public static void main(String[] args) {
-	    
-	        System.out.println("Welcome to EmployeeWage Computation");
-	        int empHrs = 0;
-	        int empwage = 0;
-	        int totalEmpWage = 0;
-	        for (int day = 0; day < NUM_OF_WORKING_DAYS; day++) {
-	            int empCheck = (int) Math.floor(Math.random() * 10) % 3;
-	            switch (empCheck) {
-	                case IS_PART_TIME:
-	                    empHrs = 4;
-	                    break;
-	                case IS_FULL_TIME:
-	                    empHrs = 8;
-	                    break;
-	                default:
-	                    empHrs = 0;
-	            }
-	            empwage = empHrs * EMP_RATE_PER_HOUR;
-	            System.out.println("Emp wage: " + empwage);
-	        }
-	        System.out.println("Total Emp Wage" + totalEmpWage);
-	    }
-	}
+    public static void main(String[] args) {
+        System.out.println("Welcome to EmployeeWage Computation");
+        int empHrs = 0;
+        int totalEmpHrs = 0;
+        int totalWorkingDays = 0;
+        while (totalEmpHrs<=MAX_HRS_IN_MONTH && totalWorkingDays<NUM_OF_WORKING_DAYS){
+            totalWorkingDays++;
+            int empCheck = (int) Math.floor(Math.random() * 10) % 3;
+            switch (empCheck) {
+                case IS_PART_TIME:
+                    empHrs = 4;
+                    break;
+                case IS_FULL_TIME:
+                    empHrs = 8;
+                    break;
+                default:
+                    empHrs = 0;
+            }
+            totalEmpHrs += empHrs ;
+            System.out.println("#Day: " + totalWorkingDays+"Emp Hr:"+empHrs);
+        }
+        int totalEmpWage = totalEmpHrs * EMP_RATE_PER_HOUR;
+        System.out.println("Total Emp Wage" + totalEmpWage);
+    }
+}
